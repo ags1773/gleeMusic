976da4ddf814ee1b5e2c038fb4fc60ce15cb90a4
ags1773 <amogh1773@gmail.com>
2018-09-26 07:55:46 +0530

    getting file ext and mimeType for uploaded files using file-type

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
 const multer = require('multer')
+const fs = require('fs')
+const acceptedFormats = ['mp3', 'flac', 'wav', 'm4a', 'aac']
 const { findFileExt } = require(path.join(__dirname, '/findFileExt'))
 const upload = multer({dest: path.join(__dirname, '/music')})
 const app = express()
@@ -16,24 +18,30 @@ app.post('/music', upload.single('song'), fileDetails, (req, res) => {
 })
 
 function fileDetails (req, res, next) {
-  let fileExt = findFileExt(path.join(__dirname, '/music/', req.file.filename))
-  if (fileExt) {
-    let {ext, mime} = fileExt
-    req.fileDetailsObj = {
-      name: req.file.encoding,
-      filename: req.file.filename,
-      mimetype: req.file.mimetype,
-      originalname: req.file.originalname,
-      path: req.file.path,
-      size: req.file.size,
-      encoding: req.file.encoding,
-      destination: req.file.destination,
-      ext: ext,
-      mime: mime
+  if (req.file) {
+    const fileExt = findFileExt(path.join(__dirname, '/music/', req.file.filename))
+
+    if (fileExt && acceptedFormats.includes(fileExt.ext)) {
+      let {ext, mime} = fileExt
+      req.fileDetailsObj = {
+        filename: req.file.filename,
+        name: req.file.encoding,
+        mimetype: req.file.mimetype,
+        originalname: req.file.originalname,
+        path: req.file.path,
+        size: req.file.size,
+        encoding: req.file.encoding,
+        destination: req.file.destination,
+        ext: ext,
+        mime: mime
+      }
+      next()
+    } else {
+      console.log('Delete file >>', path.join(__dirname, '/music/', req.file.filename))
+      fs.unlink(path.join(__dirname, '/music/', req.file.filename), err => { if (err) throw err })
+      res.status(400).json(`Bad request. Supported formats: ${acceptedFormats}`).send()
     }
-    next()
-  }
-  res.status(400).send()
+  } else res.status(400).json(`Bad request. File not recieved`).send()
 }
 
 module.exports = app
