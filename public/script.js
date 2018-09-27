a537d23f238f21794a31b94124ac04d53ff1f93a
ags1773 <amogh1773@gmail.com>
2018-09-27 22:23:24 +0530

    Added background color for now-playing song

--- public/script.js
+++ public/script.js
@@ -1,14 +1,12 @@
 const playlistItems = document.querySelectorAll('.playlist-item')
 const player = document.getElementById('player')
+let previousItem = null
 
 playlistItems.forEach(item => {
-  item.addEventListener('mouseenter', (e) => {
-    if (!item.classList.contains('test')) item.classList.add('test')
-  })
-  item.addEventListener('mouseleave', (e) => {
-    if (item.classList.contains('test')) item.classList.remove('test')
-  })
   item.addEventListener('click', (e) => {
+    if (previousItem) previousItem.classList.remove('playing')
+    previousItem = item
+    item.classList.add('playing')
     playSong(item.getAttribute('data-fileName'), item.getAttribute('data-serverIp'))
   })
 })
